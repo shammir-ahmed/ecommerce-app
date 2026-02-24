b17254e953d938206322ecdb94768d5541b0d98c
shammir-ahmed <mdshammirahmedanik@gmail.com>
2026-02-24 15:38:00 +0600

    All Web Route Add here

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\AuthWebController;
+use App\Http\Controllers\Web\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect('/login'));
+
+Route::get('/login', [AuthWebController::class, 'showLogin'])->name('login.form');
+Route::post('/login', [AuthWebController::class, 'login'])->name('login.do');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::post('/logout', [AuthWebController::class, 'logout'])->name('logout');
